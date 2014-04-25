39de2d067c467e819f060daa1920004f8f657ffc
dddotcom <ddortega@bu.edu>
2014-04-25 11:28:33 -0400

    cleaned up code

--- src/com/example/micromediaplayer/MainActivity.java
+++ src/com/example/micromediaplayer/MainActivity.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
@@ -19,6 +20,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,16 +46,14 @@ public class MainActivity extends Activity {
 	InputStream mmInputStream;
 	Thread workerThread;
 	byte[] readBuffer;
-	int readBufferPosition; 
-	int counter; 
+	int readBufferPosition;
+	int counter;
 	volatile boolean stopWorker;
 	BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
 	private Set<BluetoothDevice> pairedDevices;
 	BluetoothDevice mmDevice;
+	BluetoothSocket mmSocket;
 	AlertDialog alertDialogStores;
-
-//	String address = "000666662fb6";
-//	BluetoothDevice msp430 = bluetooth.getRemoteDevice(address);
 
 	ToggleButton BT;
 	RadioButton song1, song2, song3;
@@ -65,15 +65,14 @@ public class MainActivity extends Activity {
 	EditText bpmInput;
 
 	Integer closest = -1;
-	ArrayList<Integer> bpmArray = new ArrayList<Integer>(Arrays.asList(130,
-			125, 119, 30, 60, 90));
+	ArrayList<Integer> bpmArray = new ArrayList<Integer>(Arrays.asList(130,125, 119, 146, 85, 102));
 	ArrayList<String> songTitleArray = new ArrayList<String>(
 			Arrays.asList("Bang Bang - will.i.am", "All We Got - Fergie",
-					"Lose Yourself to Dance - Daft Punk ", "jt", "mirrors",
-					"that girl"));
+					"Lose Yourself to Dance - Daft Punk ", "Here it goes again - OkGo", "Better - Panic",
+					"Suit & Tie - JT"));
 	ArrayList<Integer> mp3Array = new ArrayList<Integer>(Arrays.asList(
-			R.raw.allwegot, R.raw.bangbang, R.raw.givelife, R.raw.jt,
-			R.raw.mirrors, R.raw.thatgirl));
+			R.raw.allwegot, R.raw.bangbang, R.raw.givelife, R.raw.okgo,
+			R.raw.panic, R.raw.jt));
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +82,6 @@ public class MainActivity extends Activity {
 		// volume adjusts with phone volume buttons
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-//		lv = (ListView)findViewById(R.id.listView1);
 		// player controls
 		stop = (Button) findViewById(R.id.stop);
 		pairedDevicesButton = (Button) findViewById(R.id.paired_devices);
@@ -91,7 +89,6 @@ public class MainActivity extends Activity {
 
 		// text views
 		BPM = (TextView) findViewById(R.id.BPM);
-		// BPMvalue = (TextView) findViewById(R.id.BPMvalue);
 		songTitle = (TextView) findViewById(R.id.songTitle);
 		bpmInput = (EditText) findViewById(R.id.editText1);
 
@@ -104,17 +101,16 @@ public class MainActivity extends Activity {
 			return; // add this to prevent from doing unnecessary stuffs
 		}
 	}
-	
-	public void addListenerOnButton(){
+
+	public void addListenerOnButton() {
 		BT = (ToggleButton) findViewById(R.id.BT);
 		BT.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				if (BT.isChecked()){
+				if (BT.isChecked()) {
 					turnOnBt();
-				}
-				else{
+				} else {
 					turnOffBt();
 				}
 			}
@@ -123,18 +119,8 @@ public class MainActivity extends Activity {
 
 	public void setUpBluetoooth() {
 		if (bluetooth != null) {
-			// Toast.makeText(this, "Continue with bluetooth setup",
-			// Toast.LENGTH_LONG).show();
 			System.out.println("Continue with bluetooth setup");
 		}
-//		if (bluetooth.isEnabled()) {
-//			Toast.makeText(this, "BT on.", Toast.LENGTH_LONG).show();
-//		} else {
-//			Intent turnon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//			startActivityForResult(turnon, 0);
-//			Toast.makeText(this, "Disabled -->  BT Turned on",
-//					Toast.LENGTH_LONG).show();
-//		}
 
 		String status;
 		if (bluetooth.isEnabled()) {
@@ -142,198 +128,204 @@ public class MainActivity extends Activity {
 			String mydeviceaddress = bluetooth.getAddress();
 			bluetooth.setName("bpmApp");
 			String mydevicename = bluetooth.getName();
-			status = mydevicename + ":" + mydeviceaddress + ":"
-					+ bluetooth.getState();
+//			status = mydevicename + ":" + mydeviceaddress + ":"
+//					+ bluetooth.getState();
 		} else {
-			status = "Bluetooth is not Enabled.";
+//			status = "Bluetooth is not Enabled.";
 			BT.setChecked(false);
 		}
-		Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+//		Toast.makeText(this, status, Toast.LENGTH_LONG).show();
 	}
-	
-	public void turnOnBt(){
-		if (bluetooth.isEnabled()) {
-//			Toast.makeText(this, "BT on.", Toast.LENGTH_LONG).show();
-		} 
-		else {
+
+	public void turnOnBt() {
+		if (!bluetooth.isEnabled()){
 			Intent turnon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(turnon, 0);
-//			Toast.makeText(this, "Disabled -->  BT Turned on", Toast.LENGTH_LONG).show();
 		}
 	}
-	
-	public void turnOffBt(){
+
+	public void turnOffBt() {
 		bluetooth.disable();
-//		Toast.makeText(this, "BT off." , Toast.LENGTH_LONG).show();
 	}
 
 	public void listPairedDevices(View view) {
 		pairedDevices = bluetooth.getBondedDevices();
 		ArrayList<String> list = new ArrayList<String>();
 
-		if (pairedDevices.size() > 0){
+		if (pairedDevices.size() > 0) {
 			for (BluetoothDevice bt : pairedDevices) {
-				if (bt.getName().equals("RNBT-2FB6")){
+				if (bt.getName().equals("RNBT-2FB6")) {
 					list.add(bt.getName());
 				}
 			}
-//			Toast.makeText(getApplicationContext(), list.get(0), Toast.LENGTH_LONG).show();
-			final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-//			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//			builder.setTitle("Adapter alert");
-			
+			// Toast.makeText(getApplicationContext(), list.get(0),
+			// Toast.LENGTH_LONG).show();
+			final ArrayAdapter adapter = new ArrayAdapter(this,
+					android.R.layout.simple_list_item_1, list);
+
 			ListView listViewItems = new ListView(this);
 			listViewItems.setAdapter(adapter);
-			
-			
-			
-			
+
 			listViewItems.setOnItemClickListener(new OnItemClickListener() {
-				public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-					
-					 Context context = view.getContext();
-						//Toast.makeText(context, "Attempt connection to msp" , Toast.LENGTH_LONG).show();
-						connectToMsp(view);
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+
+					Context context = view.getContext();
+					// Toast.makeText(context, "Attempt connection to msp" ,
+					// Toast.LENGTH_LONG).show();
+					connectToMsp(view);
 				}
 			});
-			
-			
+
 			alertDialogStores = new AlertDialog.Builder(MainActivity.this)
-				.setView(listViewItems)
-				.setTitle("Bonded Devices")
-				.show();
-//			lv.setAdapter(adapter);
-		
-			
-			 
+					.setView(listViewItems).setTitle("Bonded Devices").show();
+
+		} else {
+			Toast.makeText(getApplicationContext(), "No Paired Devices :(",
+					Toast.LENGTH_SHORT).show();
 		}
-		else{
-			Toast.makeText(getApplicationContext(), "No Paired Devices :(", Toast.LENGTH_SHORT).show();	
-		}
-		
-//		
+
+		//
 	}
-	
-	public void beginListenForData(View view){
-		
-//		final Context context = view.getContext();
-		
+
+	public void beginListenForData(View view) {
+
 		final Handler handler = new Handler();
 		final byte delimeter = 88;
-		
-		stopWorker = false; 
-		readBufferPosition = 0; 
+
+		stopWorker = false;
+		readBufferPosition = 0;
 		readBuffer = new byte[1024];
-		workerThread = new Thread(new Runnable()
-		{
-			public void run()
-			{
-				while(!Thread.currentThread().isInterrupted() && !stopWorker)
-				{
-					
+		workerThread = new Thread(new Runnable() {
+			public void run() {
+				while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+
 					try {
 						int bytesAvailable = mmInputStream.available();
-					
-					if(bytesAvailable > 0)
-					{
-						byte[] packetBytes = new byte[bytesAvailable];
-						mmInputStream.read(packetBytes);
-						for(int i=0; i <bytesAvailable; i++)
-						{
-//							Toast.makeText(context, "reading bytes" , Toast.LENGTH_LONG).show();
-							byte b = packetBytes[i];
-							if (b == delimeter)
-							{
-//								Toast.makeText(context, "all bytes read" , Toast.LENGTH_LONG).show();
-								byte[] encodedBytes = new byte[readBufferPosition];
-							     System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-							     final String data = new String(encodedBytes, "US-ASCII");
-							     readBufferPosition = 0;
-							     
-							     handler.post(new Runnable() {
-									
-									@Override
-									public void run() {
-//										Toast.makeText(context, "printing bytes" , Toast.LENGTH_LONG).show();
-										bpmInput.setText(data);
-//										songTitle.setText(data);
-										chooseBpmFromDesiredBpm();
-										if (closest > 0) {
-											Integer closestIndex = bpmArray.indexOf(closest);
-											chooseSong(closestIndex);
+
+						if (bytesAvailable > 0) {
+							byte[] packetBytes = new byte[bytesAvailable];
+							mmInputStream.read(packetBytes);
+							for (int i = 0; i < bytesAvailable; i++) {
+								// Toast.makeText(context, "reading bytes" ,
+								// Toast.LENGTH_LONG).show();
+								byte b = packetBytes[i];
+								if (b == delimeter) {
+									// Toast.makeText(context, "all bytes read"
+									// , Toast.LENGTH_LONG).show();
+									byte[] encodedBytes = new byte[readBufferPosition];
+									System.arraycopy(readBuffer, 0,
+											encodedBytes, 0,
+											encodedBytes.length);
+									final String data = new String(
+											encodedBytes, "US-ASCII");
+									readBufferPosition = 0;
+
+									handler.post(new Runnable() {
+
+										@Override
+										public void run() {
+											// Toast.makeText(context,
+											// "printing bytes" ,
+											// Toast.LENGTH_LONG).show();
+//											data = Integer.parseInt(data.replaceAll("[\\D]", "")) + "";
+											bpmInput.setText(data);
+											// songTitle.setText(data);
+											chooseBpmFromDesiredBpm();
+											if (closest > 0) {
+												Integer closestIndex = bpmArray
+														.indexOf(closest);
+												chooseSong(closestIndex);
+											}
 										}
-									}
-								});
-							}
-							else{
-								readBuffer[readBufferPosition++] = b;
+									});
+								} else {
+									readBuffer[readBufferPosition++] = b;
+								}
 							}
 						}
-					}
 					} catch (IOException e) {
-//						Toast.makeText(context, "stopping work" , Toast.LENGTH_LONG).show();
+						// Toast.makeText(context, "stopping work" ,
+						// Toast.LENGTH_LONG).show();
 						stopWorker = true;
 					}
 				}
 			}
 		});
-		
+
 		workerThread.start();
-		
+
 	}
 
 	public void connectToMsp(View view) {
 		pairedDevices = bluetooth.getBondedDevices();
-		 if(pairedDevices.size() > 0)
-	        {
-	            for(BluetoothDevice device : pairedDevices)
-	            {
-	                if(device.getName().equals("RNBT-2FB6")) //Note, you will need to change this to match the name of your device
-	                {
-	                	//Toast.makeText(this, "Found RNBT-2FB6" , Toast.LENGTH_LONG).show();
-	                    mmDevice = device;
-	                    break;
-	                }
-	            }
-	        }
-		 
-		  UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
-	      BluetoothSocket mmSocket;
-	      
-			try {
-				mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
-				mmSocket.connect();
-				Toast.makeText(this, "Connected RNBT-2FB6!" , Toast.LENGTH_LONG).show();
-		        mmOutputStream = mmSocket.getOutputStream();
-		        mmInputStream = mmSocket.getInputStream();
-		        beginListenForData(view);
-		        songTitle.setText("Listening...");
-		        
-			} catch (IOException e) {
-				Toast.makeText(this, "ERR could not connect" , Toast.LENGTH_LONG).show();
-				e.printStackTrace();
+		if (pairedDevices.size() > 0) {
+			for (BluetoothDevice device : pairedDevices) {
+				if (device.getName().equals("RNBT-2FB6")) // Note, you will need
+															// to change this to
+															// match the name of
+															// your device
+				{
+					// Toast.makeText(this, "Found RNBT-2FB6" ,
+					// Toast.LENGTH_LONG).show();
+					mmDevice = device;
+					break;
+				}
 			}
-	        
-		
+		}
+
+		UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
+																				// SerialPortService
+																				// ID
+
+		// BluetoothServerSocket tmp = null;
+		try {
+			mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+
+			e1.printStackTrace();
+		}
+
+		try {
+			// mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+			mmSocket.connect();
+			Toast.makeText(this, "Connected RNBT-2FB6!", Toast.LENGTH_LONG)
+					.show();
+			mmOutputStream = mmSocket.getOutputStream();
+			mmInputStream = mmSocket.getInputStream();
+			beginListenForData(view);
+			songTitle.setText("Listening...");
+
+		} catch (IOException e) {
+			try {
+				mmSocket.close();
+				songTitle.setText("No song currently playing...");
+				Toast.makeText(this, "ERR could not connect", Toast.LENGTH_LONG)
+						.show();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
 	}
 
 	public void onClick(View view) {
-		
-		switch(view.getId()){
+
+		switch (view.getId()) {
 		case R.id.stop:
 			if (player.isPlaying()) {
 				player.stop();
-				// radioGroup1.clearCheck();
+				player.release();
 			}
 			setUpBluetoooth();
 			break;
 		case R.id.paired_devices:
-			//listPairedDevices(view);
 			connectToMsp(view);
 			break;
 		}
-		
-		
+
 	}
 
 	public void addOnCheckedChangeListener() {
@@ -353,7 +345,12 @@ public class MainActivity extends Activity {
 	}
 
 	public void chooseSong(Integer index) {
+		if(index == null || index < 0 || index > 5){
+			return;
+		}
 		player.stop();
+		player.release();
+		Log.w("CHOOSE SONG ERROR, index = ", index + "");
 		player = MediaPlayer.create(MainActivity.this, mp3Array.get(index));
 		player.start();
 		BPM.setText("BPM: " + bpmArray.get(index) + "");
@@ -381,17 +378,6 @@ public class MainActivity extends Activity {
 					if (closest > 0) {
 						Integer closestIndex = bpmArray.indexOf(closest);
 						chooseSong(closestIndex);
-						// switch (closestIndex){
-						// case 0:
-						// song1.setChecked(true);
-						// break;
-						// case 1:
-						// song2.setChecked(true);
-						// break;
-						// case 2:
-						// song3.setChecked(true);
-						// break;
-						// }
 					}
 
 					return true;
@@ -405,7 +391,10 @@ public class MainActivity extends Activity {
 	}
 
 	public Integer chooseBpmFromDesiredBpm() {
-		int despiredBpm = Integer.parseInt(bpmInput.getText().toString());
+		if (bpmInput.getText().toString() == null || bpmInput.getText().toString().isEmpty()){
+			bpmInput.setText("1");
+		}
+		int despiredBpm = Integer.parseInt( (bpmInput.getText().toString()).replaceAll("[\\D]", ""));
 		int offset = Integer.MAX_VALUE;
 		// search for closest matching bpm in bpmArray
 		for (Integer bpm : bpmArray) {
@@ -448,9 +437,16 @@ public class MainActivity extends Activity {
 
 		// release all the media players
 		player.release();
+		bluetooth.disable();
 
+		try {
+			mmSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// wrap-up the app
-		// if (mBluetoothService != null) mBluetoothService.stop();
+//		 if (bl != null) mBluetoothService.stop();
 		finish();
 	}
 
